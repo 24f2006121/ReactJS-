752330bac8c891768cf661cfa93804a6f96cda3b
omkumar2 <omkumariitbomboy@gmail.com>
2026-01-31 13:09:09 +0530

    feat: initialize React project with Vite, add essential files and configurations
    - Created package.json with dependencies and scripts for development
    - Added Vite configuration for React
    - Implemented basic App component with styles
    - Included SVG assets for React and Vite logos
    - Set up global styles and root structure
    - Established folder structure for src and public directories
    - Documented course syllabus for a comprehensive React learning path

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 id='hello'>hello</h1>
+      <h2 id='go'>app</h2>
+      <div>
+        <p>this is a sample app.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-#hello
+# For createing the folder use this command 
+
+npm create vite
+
+# for runing the project write this command 
+
+npm run dev
+
+
+
+
+
+
+
